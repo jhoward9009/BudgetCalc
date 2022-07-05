cbd8221173842915ab0c0d4830138c6721993b4b
jhoward9009 <j.how9009@gmail.com>
2022-07-05 04:19:24 -0400

    started service worker template

---
+++ public/service-worker.js
@@ -0,0 +1,22 @@
+const APP_PREFIX = 'BUDGETCALC-'
+const VERSION = 'version_1.0'
+const CACHE_NAME = APP_PREFIX + VERSION
+const FILES_TO_CACHE = [
+    './index.html',
+    './js/index.js',
+    './css/styles.css'
+]
+
+self.addEventListener('fetch', function (e){
+
+})
+
+
+self.addEventListener('install', function (e){
+    
+})
+
+
+self.addEventListener('activiate', function (e){
+    
+})
