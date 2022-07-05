1b75b10d4bc49632c3641ec5ce958be61a20373e
jhoward9009 <j.how9009@gmail.com>
2022-07-05 04:30:53 -0400

    updated service worker fil

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,16 +7,45 @@ const FILES_TO_CACHE = [
     './css/styles.css'
 ]
 
-self.addEventListener('fetch', function (e){
-
+self.addEventListener('fetch', function (event){
+    event.respondWith(
+        caches.match(e.request).then(function(request){
+            // if (request){
+            //     return request
+            // }else{
+            //     return fetch(e.request)
+            // }
+            return request || fetch(e.request)
+        })
+    )
 })
 
 
-self.addEventListener('install', function (e){
-    
+self.addEventListener('install', function (event){
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
 })
 
 
-self.addEventListener('activiate', function (e){
-    
+self.addEventListener('activiate', function (event){
+    event.waitUntil(
+        caches.keys().then(function (keys) {
+            let keeper = keys.filter(function (key) {
+                return key.indexOf(APP_PREFIX)
+            })
+
+            keeper.push(CACHE_NAME)
+
+            return Promise.all(
+                keys.map(function (key, i) {
+                    if (keeper.indexOf(key) === -1) {
+                        return caches.delete(keys[i])
+                    }
+                })
+            )
+        })
+    )
 })
